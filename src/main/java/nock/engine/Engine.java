08609c33c403390142f15e80ffdbfb79a81e5afe
Felipe Pina <felipe.pina@protonmail.com>
2017-03-02 03:02:48 -0300

    #17 Nock engine For #17 - added engine

---
+++ src/main/java/nock/engine/Engine.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2017 Felipe Pina
+ *
+ * MIT License
+ */
+package nock.engine;
+
+/**
+ * Interpreter engine.
+ * @author Felipe Pina (felipe.pina@toptal.com)
+ * @version $Id$
+ * @since 1.0.0
+ */
+public interface Engine {
+
+}
---
+++ src/main/java/nock/engine/NockEngine.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2017 Felipe Pina
+ *
+ * MIT License
+ */
+package nock.engine;
+
+/**
+ * Nock {@link Engine}.
+ * @author Felipe Pina (felipe.pina@toptal.com)
+ * @version $Id$
+ * @since 1.0.0
+ */
+public final class NockEngine implements Engine {
+}
