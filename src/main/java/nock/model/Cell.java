ea93a78487f2236a7d4f17c75a1d6838ad83cb39
Felipe Pina <felipe.pina@protonmail.com>
2017-03-02 03:05:44 -0300

    - cell left/right

--- src/main/java/nock/model/Cell.java
+++ src/main/java/nock/model/Cell.java
@@ -12,4 +12,17 @@ package nock.model;
  * @since 1.0.0
  */
 public interface Cell extends Noun {
+
+    /**
+     * Cell's left side.
+     * @return Noun
+     */
+    Noun left();
+
+    /**
+     * Cell's right side.
+     * @return Noun
+     */
+    Noun right();
+
 }
