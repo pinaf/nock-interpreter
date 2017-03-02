68c182d1331776de41d5d0db0ea4e30f04528503
Felipe Pina <felipe.pina@protonmail.com>
2017-03-02 02:55:27 -0300

    #9 Model interfaces For #9 - model interfaces

---
+++ src/main/java/nock/model/Cell.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2017 Felipe Pina
+ *
+ * MIT License
+ */
+package nock.model;
+
+/**
+ * Cell.
+ * @author Felipe Pina (felipe.pina@toptal.com)
+ * @version $Id$
+ * @since 1.0.0
+ */
+public interface Cell extends Noun {
+}
